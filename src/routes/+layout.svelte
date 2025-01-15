259055fe4c0d4725c1cc56b6b21ca85d952ebdf5
Aaron Dye Jr <dyeaaronjr@proton.me>
2025-01-15 05:57:21 -0500

    Navbar is done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
     let { children } = $props();
 
     import {AppBar, Tooltip, Popover} from "@skeletonlabs/skeleton-svelte";
-    import {BookOpen, Menu, Folder} from "lucide-svelte";
+    import {BookOpen, Menu, Folder, Brain, Mail} from "lucide-svelte";
     import { onMount } from 'svelte';
 
     let menuState = $state(false);
@@ -14,8 +14,6 @@
         window.addEventListener('scroll', ()=> {
             const header = document.getElementById('header');
             const currentScrollY = window.scrollY;
-
-            console.log([lastScrollY, currentScrollY]);
 
             if(currentScrollY > lastScrollY) {
                 // @ts-ignore
@@ -56,16 +54,19 @@
                           positioning={{ placement: "bottom" }}
                           triggerBase="btn preset-tonal"
                           contentBase="card bg-surface-200-800 p-4 max-w-[320px]"
-                          arrow
-                          arrowBackground="!bg-surface-200 dark:!bg-surface-800"
                           classes="block md:hidden"
+                          positionerClasses="mt-2"
                         >
                             {#snippet trigger()}<Menu/>{/snippet}
                             {#snippet content()}
                                 <div>
                                     <a class="btn w-full" href="#about" onclick={closeMenu}>About</a>
                                     <hr class="divider" />
+                                    <a class="btn w-full" href="#skills" onclick={closeMenu}>Skills</a>
+                                    <hr class="divider" />
                                     <a class="btn w-full" href="#projects" onclick={closeMenu}>Projects</a>
+                                    <hr class="divider" />
+                                    <a class="btn w-full" href="#contact" onclick={closeMenu}>Contact</a>
                                 </div>
                             {/snippet}
                         </Popover>
@@ -85,66 +86,32 @@
                               openDelay={200}
                               closeDelay={100}
                             >
+                                {#snippet trigger()}<a href="#skills" class="btn"><Brain /></a>{/snippet}}
+                                {#snippet content()}<h1>Skills</h1>{/snippet}
+                            </Tooltip>
+                            <Tooltip
+                              positioning={{placement: "bottom"}}
+                              contentBase="card preset-filled p-2"
+                              openDelay={200}
+                              closeDelay={100}
+                            >
                                 {#snippet trigger()}<a href="#projects" class="btn"><Folder /></a>{/snippet}}
                                 {#snippet content()}<h1>Projects</h1>{/snippet}
+                            </Tooltip>
+                            <Tooltip
+                              positioning={{placement: "bottom"}}
+                              contentBase="card preset-filled p-2"
+                              openDelay={200}
+                              closeDelay={100}
+                            >
+                                {#snippet trigger()}<a href="#contact" class="btn"><Mail /></a>{/snippet}}
+                                {#snippet content()}<h1>Contact Me</h1>{/snippet}
                             </Tooltip>
                         </div>
                     {/snippet}
                 </AppBar>
     </header>
-    <main class="p-4 pt-24 absolute">
+    <main class="p-4 pt-24 absolute w-full">
         {@render children()}
     </main>
 </div>
-
-<!--<div id="header" class="w-full flex items-center justify-center p-0 md:p-4 fixed">-->
-<!--    <AppBar classes="w-full md:w-[40rem] rounded-none md:rounded-2xl shadow-xl px-4">-->
-<!--        {#snippet lead()}-->
-<!--            <a class="text-2xl" href="/">Aaron Dye</a>-->
-<!--        {/snippet}-->
-<!--        {#snippet trail()}-->
-<!--            <Popover-->
-<!--              bind:open={menuState}-->
-<!--              positioning={{ placement: "bottom" }}-->
-<!--              triggerBase="btn preset-tonal"-->
-<!--              contentBase="card bg-surface-200-800 p-4 max-w-[320px]"-->
-<!--              arrow-->
-<!--              arrowBackground="!bg-surface-200 dark:!bg-surface-800"-->
-<!--              classes="block md:hidden"-->
-<!--            >-->
-<!--                {#snippet trigger()}<Menu/>{/snippet}-->
-<!--                {#snippet content()}-->
-<!--                    <div>-->
-<!--                        <a class="btn w-full" href="#about" onclick={closeMenu}>About</a>-->
-<!--                        <hr class="divider" />-->
-<!--                        <a class="btn w-full" href="#projects" onclick={closeMenu}>Projects</a>-->
-<!--                    </div>-->
-<!--                {/snippet}-->
-<!--            </Popover>-->
-<!--            <div class="hidden md:block">-->
-<!--                <Tooltip-->
-<!--                  positioning={{placement: "bottom"}}-->
-<!--                  contentBase="card preset-filled p-2"-->
-<!--                  openDelay={200}-->
-<!--                  closeDelay={100}-->
-<!--                >-->
-<!--                    {#snippet trigger()}<a href="#about" class="btn"><BookOpen /></a>{/snippet}}-->
-<!--                    {#snippet content()}<h1>About Me</h1>{/snippet}-->
-<!--                </Tooltip>-->
-<!--                <Tooltip-->
-<!--                  positioning={{placement: "bottom"}}-->
-<!--                  contentBase="card preset-filled p-2"-->
-<!--                  openDelay={200}-->
-<!--                  closeDelay={100}-->
-<!--                >-->
-<!--                    {#snippet trigger()}<a href="#projects" class="btn"><Folder /></a>{/snippet}}-->
-<!--                    {#snippet content()}<h1>Projects</h1>{/snippet}-->
-<!--                </Tooltip>-->
-<!--            </div>-->
-<!--        {/snippet}-->
-<!--    </AppBar>-->
-<!--</div>-->
-
-<!--<div class="p-4 absolute">-->
-<!--    {@render children()}-->
-<!--</div>-->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,13 @@
-<div id="projects" class="h-screen">
-	<h1>Hello</h1>
-</div>
-<div id="about" class="h-screen">
-	<h1>I love dogs</h1>
-</div>
+
+<section id="about" class="flex h-screen justify-center items-center">
+	<h1>About</h1>
+</section>
+<section id="skills" class="flex h-screen justify-center items-center">
+	<h1>Skills</h1>
+</section>
+<section id="projects" class="flex h-screen justify-center items-center">
+	<h1>Projects</h1>
+</section>
+<section id="contact" class="flex h-screen justify-center items-center">
+	<h1>Contact</h1>
+</section>
